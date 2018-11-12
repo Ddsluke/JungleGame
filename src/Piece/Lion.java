3e4257b905079b983e67be70885a18a2d07397cb
Ddsluke <35838366+Ddsluke@users.noreply.github.com>
2018-11-12 17:14:04 +0800

    add Lion and Tiger move function

--- src/Piece/Lion.java
+++ src/Piece/Lion.java
@@ -1,8 +1,28 @@
 package Piece;
 
+import Board.GameBoard;
+
 public class Lion extends Piece {
     public Lion(int x, int y, int side){
         super(x, y, side);
         rank = 7;
+    }
+
+    public void move(int newXPosition, int newYPosition, GameBoard gameBoard){
+        if (gameBoard.isRiver(newXPosition, newYPosition)){// If the new position is type RIVER then return.
+            return;
+        }
+        else if (!(gameBoard.isAcrossWater(xPosition, yPosition, newXPosition, newYPosition) || gameBoard.isAdj(xPosition,yPosition,newXPosition,newYPosition))){
+            return;
+        }
+        Piece other = gameBoard.getPiece(newXPosition, newYPosition);
+        if (other != null){ // if there is a piece on it
+            if (ableToCapture(other)){
+                gameBoard.setPiece(newXPosition, newYPosition, this);
+            }
+        }
+        else {// There is no piece on it and we can directly move to the new position
+            gameBoard.setPiece(newXPosition, newYPosition, this);
+        }
     }
 }
--- src/Piece/Tiger.java
+++ src/Piece/Tiger.java
@@ -8,6 +8,20 @@ public class Tiger extends Piece {
         rank = 6;
     }
     public void move(int newXPosition, int newYPosition, GameBoard gameBoard){
-
+        if (gameBoard.isRiver(newXPosition, newYPosition)){// If the new position is type RIVER then return.
+            return;
+        }
+        else if (!(gameBoard.isAcrossWater(xPosition, yPosition, newXPosition, newYPosition) || gameBoard.isAdj(xPosition,yPosition,newXPosition,newYPosition))){
+            return;
+        }
+        Piece other = gameBoard.getPiece(newXPosition, newYPosition);
+        if (other != null){ // if there is a piece on it
+            if (ableToCapture(other)){
+                gameBoard.setPiece(newXPosition, newYPosition, this);
+            }
+        }
+        else {// There is no piece on it and we can directly move to the new position
+            gameBoard.setPiece(newXPosition, newYPosition, this);
+        }
     }
 }
