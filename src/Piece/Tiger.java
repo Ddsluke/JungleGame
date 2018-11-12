111fa0fc8e96d50c22e715a6f1707de14c36ec22
XU Xiaohchuan <35758677+incandescentxxc@users.noreply.github.com>
2018-11-12 22:22:34 +0800

    Update Tiger.java

--- src/Piece/Tiger.java
+++ src/Piece/Tiger.java
@@ -7,12 +7,13 @@ public class Tiger extends Piece {
         super(x, y, side);
         rank = 6;
     }
+    @Override
     public void move(int newXPosition, int newYPosition, GameBoard gameBoard){
         if (gameBoard.isRiver(newXPosition, newYPosition)){// If the new position is type RIVER then return.
             return;
         }
         else if (!(gameBoard.isAcrossWater(xPosition, yPosition, newXPosition, newYPosition) || gameBoard.isAdj(xPosition,yPosition,newXPosition,newYPosition))){
-            return;
+            return;// If the tiger neither moves to adjacent position nor jumps to the other side over the river then return.
         }
         Piece other = gameBoard.getPiece(newXPosition, newYPosition);
         if (other != null){ // if there is a piece on it
